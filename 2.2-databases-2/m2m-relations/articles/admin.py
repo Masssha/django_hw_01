4c0d3833a967ea5dd57f820dcc04e9d6e5365a75
Mariia Leonova <massha.leonova@yandex.ru>
2024-06-08 01:39:37 +0300

    some edits to database m2m

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,9 +1,35 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
 from .models import Article, Tag, Scope
+
+# вот тут я не очень поняла, что мы проверяем. что только один тег может быть главным?..
+class ScopeInlineFormset(BaseInlineFormSet):
+def clean(self):
+    for form in self.forms:
+        # В form.cleaned_data будет словарь с данными
+        # каждой отдельной формы, которые вы можете проверить
+        form.cleaned_data
+        # вызовом исключения ValidationError можно указать админке о наличие ошибки
+        # таким образом объект не будет сохранен,
+        # а пользователю выведется соответствующее сообщение об ошибке
+        raise ValidationError('Тут всегда ошибка')
+    return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 2
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'text', 'published_at', 'image']
+    list_display = ['id', 'title', 'text', 'published_at', 'image']
 
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    inlines = [ScopeInline,]
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
@@ -13,13 +12,14 @@ class Article(models.Model):
         verbose_name_plural = 'Статьи'
 
 class Tag(models.Model):
-    title = models.CharField(max_length=256, verbose_name='Категория')
-    articles = models.ManyToManyField(Article, related_name='tag')
+    name = models.CharField(max_length=256, verbose_name='Категория')
+    # articles = models.ManyToManyField(Article, related_name='tag')
 
 class Scope(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scope')
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scope')
-    quantity = models.IntegerField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False)
+    title = models.CharField(max_length=256, verbose_name='Scope_title', default='почему-то это поле требовал джанго :D')
 
     def __str__(self):
         return self.title
