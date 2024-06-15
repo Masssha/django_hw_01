243af3aeb1f05678e7625a25dc90fe8a3146a372
Mariia Leonova <massha.leonova@yandex.ru>
2024-06-15 18:37:00 +0300

    homework about adminka

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,46 +1,45 @@
 from django.contrib import admin
 from django.core.exceptions import ValidationError
 from django.forms import BaseInlineFormSet
+from django import forms
 
 from .models import Article, Tag, Scope
 
+
+
+
 # вот тут я не очень поняла, как проверять. как достучаться отсюда до scope.is_main
-# class ScopeInlineFormset(BaseInlineFormSet):
-#     def clean(self):
-#         for form in self.forms:
-#             form.cleaned_data = form.dict()
-#             if form.cleaned_data.is_main == 0:
-#                 raise ValidationError('Укажите основной раздел')
-#             if form.cleaned_data.is_main > 1:
-#                 raise ValidationError('Основным может быть только один раздел')
-#             # for key, value in form.items():
-#             #     if form.is_main:
-#             #         raise ValidationError('Укажите основной раздел')
-#             #     if value > 1:
-#             #         raise ValidationError('Основным может быть только один раздел')
-#
-#         #     # В form.cleaned_data будет словарь с данными
-#         #     # каждой отдельной формы, которые вы можете проверить
-#         #     form.cleaned_data
-#             # вызовом исключения ValidationError можно указать админке о наличие ошибки
-#             # таким образом объект не будет сохранен,
-#             # а пользователю выведется соответствующее сообщение об ошибке
-#             # raise ValidationError('Основным может быть только один раздел')
-#             # raise ValidationError('Укажите основной раздел')
-#         return super().clean()  # вызываем базовый код переопределяемого метода
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        data_ismain = 0
+        tag = 0
+        for form in self.forms:
+            if form.cleaned_data['is_main']:
+                data_ismain += 1
+            if form.cleaned_data['tag']:
+                tag += 1
+        if tag == 0:
+            raise ValidationError('Укажите хотя бы один раздел')
+        if data_ismain == 0:
+            raise ValidationError('Укажите основной раздел')
+        if data_ismain != 1:
+            raise ValidationError('Основным может быть только один раздел')
+
+
+        return super().clean()
 
 
 
 class ScopeInline(admin.TabularInline):
     model = Scope
-    # formset = ScopeInlineFormset
+    formset = ScopeInlineFormset
     extra = 0
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'text', 'published_at', 'image']
-    #как в эту таблицу вытащить теги статей - тоже не поняла
+    #как в эту таблицу вытащить теги статей - тоже не поняла. мы же задавали related_name, и я думала, этого достаточно, чтобы дописать сюда 'tag'?..
     inlines = [ScopeInline, ]
 
 @admin.register(Tag)
